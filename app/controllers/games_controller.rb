b5f650a99eac0ae37c53e7fe20e3f16531a0232c
Jielun Chen <chenjielunn@gmail.com>
2019-12-09 22:58:06 +0800

    Controller and View for Longest Word Game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,26 +1,30 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = ('A'..'Z').to_a.shuffle[5, rand(6..10)].join
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
 
-raise
- @word = params[:word]
-#find a way to get letters from grid to score
-@letters = params[:letters].split
-match = @word.chars.all? do |a|
-  @letters.include? a
-end
+  private
 
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
 
-#compare submission with letter sin grid
-
-@submission = params[:submission]
-   url = "https://wagon-dictionary.herokuapp.com/"
-   api_guess = url + @submission
-   user_serialized = open(api_guess).read
-   user = JSON.parse(user_serialized)
-
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
